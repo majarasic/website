332fe9604025f9b0fa5ba49a0a9ae8f01472fc02
mario <mskoc.vd@hotmail.com>
2018-01-30 08:23:37 +0100

    404 page added

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.sendfile('./public/404.html', {
     message: err.message,
     error: {}
   });
